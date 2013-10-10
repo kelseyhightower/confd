25fb5041c54d554b1da27a588420107a0267a83e
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-10-10 05:52:04 -0700

    Add a note regarding the non portable way we gather the uid, gid, and mode of a file

--- template_resource.go
+++ template_resource.go
@@ -199,6 +199,10 @@ func fileStat(name string) (fi fileInfo, err error) {
 			return fi, err
 		}
 		stats, _ := f.Stat()
+		// Gathering the uid, gid, and mode this way is not portable.
+		// This will build on Linux but not OS X. The Mode on OS X returns a
+		// uint16 but uint32 on Linux. We could change our FileInfo Mode field
+		// type to os.FileMode and cast Mode here.
 		fi.Uid = stats.Sys().(*syscall.Stat_t).Uid
 		fi.Gid = stats.Sys().(*syscall.Stat_t).Gid
 		fi.Mode = stats.Sys().(*syscall.Stat_t).Mode
