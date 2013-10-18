c2a47c683fcef2c7c6fc3e1297009c083e2ed408
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-10-18 06:35:00 -0700

    Add tests for etcd_client.go

--- etcd_client_test.go
+++ etcd_client_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"testing"
+)
+
+var pathToKeyTests = []struct {
+	key      string
+	prefix   string
+	expected string
+}{
+	{"/nginx/port", "", "nginx_port"},
+	{"/prefix/nginx/port", "/prefix", "nginx_port"},
+	{"/nginx/worker_processes", "", "nginx_worker_processes"},
+	{"/foo/bar/mat/zoo", "", "foo_bar_mat_zoo"},
+}
+
+func TestPathToKey(t *testing.T) {
+	for _, pt := range pathToKeyTests {
+		result := pathToKey(pt.key, pt.prefix)
+		if result != pt.expected {
+			t.Errorf("Expected pathToKey(%s, %s) to == %s, got %s",
+				pt.key, pt.prefix, pt.expected, result)
+		}
+	}
+}
