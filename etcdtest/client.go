47b5799535fce17a10aa51d734ef64a5ba9f9e5d
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-10-20 09:52:15 -0700

    Update comments for etcdtest/client.go

--- etcdtest/client.go
+++ etcdtest/client.go
@@ -4,21 +4,22 @@ import (
 	"github.com/coreos/go-etcd/etcd"
 )
 
-// Client represents a fake etcd client.
+// Client represents a fake etcd client. Used for testing.
 type Client struct {
 	Responses map[string][]*etcd.Response
 }
 
+// Get mimics the etcd.Client.Get() method.
 func (c *Client) Get(key string) ([]*etcd.Response, error) {
 	return c.Responses[key], nil
 }
 
-// AddResponses adds or replaces
+// AddResponses adds or updates the Client.Responses map.
 func (c *Client) AddResponse(key string, response []*etcd.Response) {
 	c.Responses[key] = response
 }
 
-// NewClient returns a new fake etcd client.
+// NewClient returns a fake etcd client.
 func NewClient() *Client {
 	responses := make(map[string][]*etcd.Response)
 	return &Client{responses}
