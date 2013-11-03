e813187111b2326c9920fe1b24e18d0d00b5365f
Kelsey Hightower <kelsey.hightower@gmail.com>
2013-11-02 19:18:52 -0700

    Fix broken config package test

--- config/config_test.go
+++ config/config_test.go
@@ -17,7 +17,7 @@ func TestLoadConfig(t *testing.T) {
 		"", "", "/etc/confd/conf.d", []string{"http://127.0.0.1:4001"},
 		600, "/", "/etc/confd/templates",
 	}
-	loadConfig("")
+	LoadConfig("")
 	cc := ClientCert()
 	if cc != expected.clientCert {
 		t.Errorf("Expected default clientCert = %s, got %s", expected.clientCert, cc)
