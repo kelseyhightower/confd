fe3e6001d0a135538fe083fc4d7c778eca64d2c8
Kelsey Hightower <kelsey.hightower@gmail.com>
2014-06-12 01:47:56 -0700

    integration test just use localhost

--- integration/etcd/test.sh
+++ integration/etcd/test.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-etcd_host=${1}
-etcd_port=${2}
-
-# Configure consul
-curl -L -X PUT http://${etcd_host}:${etcd_port}/v2/keys/database/host -d value=127.0.0.1
-curl -L -X PUT http://${etcd_host}:${etcd_port}/v2/keys/database/password -d value=p@sSw0rd
-curl -L -X PUT http://${etcd_host}:${etcd_port}/v2/keys/database/port -d value=3306
-curl -L -X PUT http://${etcd_host}:${etcd_port}/v2/keys/database/username -d value=confd
-curl -L -X PUT http://${etcd_host}:${etcd_port}/v2/keys/upstream/app1 -d value=10.0.1.10:8080
-curl -L -X PUT http://${etcd_host}:${etcd_port}/v2/keys/upstream/app2 -d value=10.0.1.11:8080
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/database/host -d value=127.0.0.1
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/database/password -d value=p@sSw0rd
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/database/port -d value=3306
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/database/username -d value=confd
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/upstream/app1 -d value=10.0.1.10:8080
+curl -L -X PUT http://127.0.0.1:4001/v2/keys/upstream/app2 -d value=10.0.1.11:8080
 
 # Run confd
-./confd -onetime -confdir ./integration/confdir -backend etcd -node "http://${etcd_host}:${etcd_port}"
+./confd -onetime -confdir ./integration/confdir -backend etcd -node http://127.0.0.1:4001
