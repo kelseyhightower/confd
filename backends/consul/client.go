5bf38d0ee13d396a34084a30636bf55f973e4d0f
John Engelman <john.r.engelman@gmail.com>
2014-08-08 14:53:19 -0500

    support confd prefixes to reduce querying against Consul

--- backends/consul/client.go
+++ backends/consul/client.go
@@ -16,6 +16,7 @@ type Client struct {
 	catalog *consulapi.Catalog
 }
 
+// Wrapper for service information
 type Service struct {
 	Name string
 	Tags []string
@@ -47,12 +48,127 @@ func (c *Client) GetValues(keys []string) (map[string]string, error) {
 			vars[path.Join("/", p.Key)] = string(p.Value)
 		}
 	}
-	services, _ := ListServices(*c)
-	service_vars, _ := GetServicesHealth(*c, services)
-	for k, v := range service_vars {
-		vars[k] = v
+	sd, err := ProcessConsulData(*c, keys)
+	if err != nil {
+		return vars, err
+	} else {
+		MergeMaps(vars, sd)
 	}
 	return vars, nil
+}
+
+// Use the configured prefixes to load Consule data into
+// the available keys
+func ProcessConsulData(c Client, keys []string) (map[string]string, error) {
+	vars := make(map[string]string)
+	for _, key := range keys {
+		data, err := ProcessConsulDataPrefix(c, key)
+		if err != nil {
+			return vars, err
+		} else {
+			MergeMaps(vars, data)
+		}
+	}
+	return vars, nil
+}
+
+// Process a specific key with respect to Consul data
+func ProcessConsulDataPrefix(c Client, key string) (map[string]string, error) {
+	// Split the prefix into tokens so we can iterate through them
+	tokens := strings.Split(strings.TrimPrefix(key, "/"), "/")
+	// if the prefix doesn't start with /_consul, then this code won't
+	// handle it, so exit out
+	if tokens[0] != "_consul" {
+		return make(map[string]string), nil
+	}
+	// if the prefix is simply /_consul, then get all Consul data that we can
+	// and return it
+	if cap(tokens) == 1 {
+		return ProcessAllConsulServiceData(c)
+	} else {
+		// if the prefix starts with /_consul then pass it to some subfunctions
+		// for processing. Pops off the _consul token so we can ignore it
+		return ProcessConsulServiceData(c, tokens[1:])
+	}
+}
+
+// Gets data for all services in consul by looking up services from
+// the Consul catalog
+func ProcessAllConsulServiceData(c Client) (map[string]string, error) {
+	// Get all services from the Consul catalog
+	services, err := ListServices(c)
+	if err != nil {
+		return nil, err
+	}
+	// Get all the health information for the services
+	return GetServicesHealth(c, services)
+}
+
+// Gets service data from Consul by traversing the provide prefix that
+// has been tokenized
+func ProcessConsulServiceData(c Client, tokens []string) (map[string]string, error) {
+	// only handle prefixes that start with /_consul/service
+	if tokens[0] != "service" {
+		return make(map[string]string), nil
+	}
+	if cap(tokens) == 1 {
+		// If the key is /_consul/service then we get all service data
+		// by querying the Consul catalog and iterating over the list of
+		// services that are known
+		return ProcessAllConsulServiceData(c)
+	} else {
+		// Otherwise, pop this token off and try processing for a specific service
+		return ProcessConsulDataForService(c, tokens[1:])
+	}
+}
+
+// Use the remaing tokens in the prefix to acquire service data from Consul
+func ProcessConsulDataForService(c Client, tokens []string) (map[string]string, error) {
+	// The first token is the service name
+	service := tokens[0]
+
+	// If there is a second token, than it is the tag
+	tag := ""
+	if cap(tokens) > 1 {
+		tag = tokens[1]
+	}
+	// Get the Service from the Consul catalog
+	srv, err := GetService(c, service)
+	if err != nil {
+		return nil, err
+	}
+	// If a service by this name exists, get its health, otherwise return empty
+	if srv.Name != "" {
+		if tag == "" {
+			// If there is no tag, then get health for all tags
+			return GetServiceHealth(c, srv)
+		} else {
+			// Otherwise just get the health for the specfic tag
+			return GetServiceTagHealth(c, srv, tag)
+		}
+	} else {
+		return make(map[string]string), nil
+	}
+}
+
+// Get a specific service from the Consul catalog.
+// Returns a zero struct if not found...Check that the service.Name != ""
+func GetService(c Client, name string) (Service, error) {
+	// Get all services in the catalog
+	services, err := ListServices(c)
+	// return error if it occurred
+	if err != nil {
+		return Service{}, err
+	}
+	// Find the service instance with the name we are looking for and return it
+	// Otherwise return the zero struct
+	var service Service
+	for _, srv := range services {
+		if srv.Name == name {
+			service = srv
+		}
+	}
+	return service, nil
 }
 
 // ListServices queries Consul for all registered services
@@ -66,6 +182,19 @@ func ListServices(c Client) ([]Service, error) {
 		services = append(services, Service{name, tags})
 	}
 	return services, nil
+}
+
+// Retrieve data for all provided services
+func GetServicesHealth(c Client, services []Service) (map[string]string, error) {
+	entries := make(map[string]string)
+	for _, service := range services {
+		service_entries, err := GetServiceHealth(c, service)
+		if err != nil {
+			return entries, err
+		}
+		MergeMaps(entries, service_entries)
+	}
+	return entries, nil
 }
 
 // GetServicesHealth queries Consul for the active host data for each
@@ -85,34 +214,62 @@ func ListServices(c Client) ([]Service, error) {
 // subset of instances of that service with a particular tag.
 //
 // Currently Consul datacenters are not supported via this method.
-func GetServicesHealth(c Client, services []Service) (map[string]string, error) {
+func GetServiceHealth(c Client, service Service) (map[string]string, error) {
 	entries := make(map[string]string)
-	for _, entry := range services {
-		// For each service retrieve the list of health hosts from Consul
-		serviceEntries, _, err := c.health.Service(entry.Name, "", true, nil)
+	// For each service retrieve the list of health hosts from Consul
+	serviceEntries, _, err := c.health.Service(service.Name, "", true, nil)
+	if err != nil {
+		return entries, err
+	}
+	// For each service host, add the JSON data to the root list
+	for idx, serviceEntry := range serviceEntries {
+		key := path.Join("/", "_consul", "service", service.Name, strconv.Itoa(idx))
+		service_json, _ := json.Marshal(serviceEntry)
+		entries[key] = string(service_json)
+	}
+	// For each tag registered for the service, retrieve the healthy hosts
+	// for the service & tag
+	tag_entries, err := GetServiceTagsHealth(c, service)
+	if err != nil {
+		return entries, err
+	}
+	MergeMaps(entries, tag_entries)
+	return entries, nil
+}
+
+// Get service data for all Tags for a service
+func GetServiceTagsHealth(c Client, service Service) (map[string]string, error) {
+	entries := make(map[string]string)
+	for _, tag := range service.Tags {
+		tag_entries, err := GetServiceTagHealth(c, service, tag)
 		if err != nil {
 			return entries, err
 		}
-		// For each service host, add the JSON data to the root list
-		for idx, serviceEntry := range serviceEntries {
-			key := path.Join("/", "_consul", "service", entry.Name, strconv.Itoa(idx))
-			service_json, _ := json.Marshal(serviceEntry)
-			entries[key] = string(service_json)
-		}
-		// For each tag registered for the service, retrieve the healthy hosts
-		// for the service & tag
-		for _, tag := range entry.Tags {
-			serviceEntries, _, err := c.health.Service(entry.Name, tag, true, nil)
-			if err != nil {
-				return entries, err
-			}
-			// Add each service hosts for the tag, add the JSON data to the tag list
-			for idx, serviceEntry := range serviceEntries {
-				key := path.Join("/", "_consul", "service", entry.Name, tag, strconv.Itoa(idx))
-				service_json, _ := json.Marshal(serviceEntry)
-				entries[key] = string(service_json)
-			}
-		}
+		MergeMaps(entries, tag_entries)
 	}
 	return entries, nil
+}
+
+// Get service data for a single service Tag
+func GetServiceTagHealth(c Client, service Service, tag string) (map[string]string, error) {
+	entries := make(map[string]string)
+	serviceEntries, _, err := c.health.Service(service.Name, tag, true, nil)
+	if err != nil {
+		return entries, err
+	}
+	// Add each service hosts for the tag, add the JSON data to the tag list
+	for idx, serviceEntry := range serviceEntries {
+		key := path.Join("/", "_consul", "service", service.Name, tag, strconv.Itoa(idx))
+		service_json, _ := json.Marshal(serviceEntry)
+		entries[key] = string(service_json)
+	}
+	return entries, nil
+}
+
+// Merge two maps together. Data from the 2nd arg will override the data
+// in the original map.
+func MergeMaps(base map[string]string, data map[string]string) {
+	for k, v := range data {
+		base[k] = v
+	}
 }
