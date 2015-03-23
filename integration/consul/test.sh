1f70235c47074965908048eb546b954dae2e0091
Matthew Fisher <matthewf@opdemand.com>
2015-03-22 23:36:59 -0700

    remove unused service registrations

--- integration/consul/test.sh
+++ integration/consul/test.sh
@@ -8,9 +8,5 @@ curl -X PUT http://127.0.0.1:8500/v1/kv/database/username -d 'confd'
 curl -X PUT http://127.0.0.1:8500/v1/kv/upstream/app1 -d '10.0.1.10:8080'
 curl -X PUT http://127.0.0.1:8500/v1/kv/upstream/app2 -d '10.0.1.11:8080'
 
-curl -X PUT http://127.0.0.1:8500/v1/agent/service/register -d '{"ID":"rails1", "Name":"rails","Tags":["master", "v1"],"Port":8080,"Check":{}}'
-curl -X PUT http://127.0.0.1:8500/v1/agent/service/register -d '{"ID":"rails2", "Name":"rails","Tags":["master", "v2"],"Port":9090,"Check":{}}'
-curl -X PUT http://127.0.0.1:8500/v1/agent/service/register -d '{"ID":"rails3", "Name":"rails","Tags":["slave", "v2"],"Port":9999,"Check":{"Script": "curl localhost:9999", "Interval": "10s", "TTL": "15s"}}'
-
 # Run confd
 ./confd -onetime -verbose -confdir ./integration/confdir -backend consul -node "127.0.0.1:8500"
