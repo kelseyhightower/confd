9ab862e1c6c323aaee79ca6eb66e2383ec4284ee
Smit Shah <who828@gmail.com>
2015-06-11 19:19:56 +0530

    Run confd as root

--- fk-config-service-confd/usr/share/fk-config-service-confd/bin/run.sh
+++ fk-config-service-confd/usr/share/fk-config-service-confd/bin/run.sh
@@ -6,5 +6,5 @@ PACKAGE="fk-config-service-confd"
 USER="cfgsvc"
 CONFD="/usr/share/$PACKAGE/bin/confd"
 
-exec setuidgid $USER $CONFD >> /var/log/flipkart/config-service/confd-out.log 2>&1
+exec $CONFD >> /var/log/flipkart/config-service/confd-out.log 2>&1
 
