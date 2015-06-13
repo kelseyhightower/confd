3becf12fd1e3176d6227d3fcca963e3083eba460
Smit Shah <who828@gmail.com>
2015-06-14 02:30:02 +0530

    Add method to conver to json, tmpl, toml from Hash

--- confd-template-generator/lib/confd/template/generator.rb
+++ confd-template-generator/lib/confd/template/generator.rb
@@ -1,4 +1,5 @@
 require "confd/template/generator/version"
+require 'json'
 
 module Confd
   class TemplateGenerator
@@ -7,7 +8,54 @@ module Confd
       @input = input
     end
 
-    def generate_templ
+    def to_tmpl
+      recur_to_tmpl(input, {}, [])
+    end
+
+    def to_toml
+      recur_to_toml(input, [], [])
+    end
+
+    def to_json
+      recur_to_json(input, {}, []).to_json
+    end
+
+    private
+    def recur_to_json(input, output, current_keys)
+      input.each do |k, v|
+        if v.is_a?(Hash)
+          recur_to_json(v, output, current_keys + [k])
+        else
+          keys = current_keys + [k]
+          output.merge!({"#{keys.join(".")}" => v})
+        end
+      end
+      output
+    end
+
+    def recur_to_tmpl(input, output, current_keys)
+      input.each do |k, v|
+        output[k] ||= {}
+        if v.is_a?(Hash)
+          output[k].merge!(recur_to_tmpl(v, output[k], current_keys + [k]))
+        else
+          keys = current_keys + [k]
+          output.merge!({k => %Q[{{getv "/#{keys.join(".")}"}}]})
+        end
+      end
+      output
+    end
+
+    def recur_to_toml(input, current_keys, total_keys)
+      input.each do |k, v|
+        if v.is_a?(Hash)
+          recur_to_toml(v, current_keys + [k], total_keys)
+        else
+          keys = current_keys + [k]
+          total_keys << "/#{keys.join(".")}" 
+        end
+      end
+      total_keys
     end
   end
 end
--- confd-template-generator/spec/confd/template/generator_spec.rb
+++ confd-template-generator/spec/confd/template/generator_spec.rb
@@ -10,8 +10,27 @@ describe Confd::TemplateGenerator do
     expect(Confd::TemplateGenerator.new(hash).input).to eq(hash)
   end
   
-  it 'generates templ from the input hashmap' do
-    hash = {"a" => "abc"}
-    expect(Confd::TemplateGenerator.new(hash).input).to eq(hash)
+  it 'generates tmpl from the input hashmap' do
+    hash = {"a" => {"b" => "abc"}}
+    expect(Confd::TemplateGenerator.new(hash).to_tmpl).to eq({"a" => {"b" => "{{getv \"/a.b\"}}"}})
+  end
+
+  it 'generates tmpl from the complex input hashmap' do
+    hash = {"a" => {"b" => "abc", "d" => "dc"}, "c" => 4}
+    expect(Confd::TemplateGenerator.new(hash).to_tmpl).to eq({"a" => 
+                                                                    {"b" => "{{getv \"/a.b\"}}",
+                                                                    "d" => "{{getv \"/a.d\"}}"},
+                                                                     "c" => "{{getv \"/c\"}}"})
+  end
+
+  it 'extract keys from the complex input hashmap' do
+    hash = {"a" => {"b" => "abc", "d" => "dc"}, "c" => 4}
+    expect(Confd::TemplateGenerator.new(hash).to_toml).to eq(["/a.b", "/a.d", "/c"])
+  end
+
+  it 'generates JSON from the complex input hashmap' do
+    hash = {"a" => {"b" => "abc", "d" => "dc"}, "c" => 4}
+    output = {"a.b" => "abc", "a.d" => "dc", "c" => 4}.to_json
+    expect(Confd::TemplateGenerator.new(hash).to_json).to eq(output)
   end
 end
