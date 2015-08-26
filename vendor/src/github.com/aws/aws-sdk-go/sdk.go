b4f136f2679f8ac0b586b75328585ce5c04d44c7
Riccardo Freixo <riccardofreixo@gmail.com>
2015-08-26 19:38:08 +0100

    Add latest version of aws-sdk-go

---
+++ vendor/src/github.com/aws/aws-sdk-go/sdk.go
@@ -0,0 +1,207 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v149)
+"   /home/marvin/go/src/github.com/aws/aws-sdk-go
+"   Sorted by      name reversed
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:exec
+" ============================================================================
+../
+home/
+| marvin/
+| | .Skype/
+| | .adobe/
+| | .ansible/
+| | .aws/
+| | .bin/
+| | .cache/
+| | .compiz/
+| | .config/
+| | .dbus/
+| | .dosbox/
+| | .eclipse/
+| | .env/
+| | .gconf/
+| | .gervill/
+| | .gimp-2.8/
+| | .git/
+| | .gnome/
+| | .gnome2/
+| | .gnome2_private/
+| | .gnupg/
+| | .gradle/
+| | .gstreamer-0.10/
+| | .gvfs/
+| | .httpie/
+| | .ipython/
+| | .java/
+| | .jmc/
+| | .local/
+| | .macromedia/
+| | .mozilla/
+| | .netExtenderCerts/
+| | .npm/
+| | .pdfstudio10/
+| | .pip/
+| | .pki/
+| | .putty/
+| | .pylint.d/
+| | .scummvm/
+| | .ssh/
+| | .ssh_encrypted/
+| | .steam/
+| | .terraform.d/
+| | .thumbnails/
+| | .thunderbird/
+| | .usr/
+| | .vim/
+| | .visualvm/
+| | .wine/
+| | .wireshark/
+| | Desktop/
+| | DevopsWorks/
+| | Documents/
+| | Downloads/
+| | Music/
+| | Personal/
+| | Pictures/
+| | Public/
+| | Templates/
+| | Videos/
+| | VirtualBox VMs/
+| | asgmodule/
+| | elbi-config/
+| | euler/
+| | go/
+| | | src/
+| | | | launchpad.net/
+| | | | gopkg.in/
+| | | | google.golang.org/
+| | | | golang.org/
+| | | | github.com/
+| | | | | xiocode/
+| | | | | vaughan0/
+| | | | | rasa/
+| | | | | rackspace/
+| | | | | racker/
+| | | | | patrick-higgins/
+| | | | | nsf/
+| | | | | motain/
+| | | | | mitchellh/
+| | | | | kisielk/
+| | | | | jstemmer/
+| | | | | howeyc/
+| | | | | hashicorp/
+| | | | | golang/
+| | | | | going/
+| | | | | davecgh/
+| | | | | constabulary/
+| | | | | aws/
+| | | | | | aws-sdk-go/
+| | | | | | | sdk.go
+| | | | | | | README.md
+| | | | | | | NOTICE.txt
+| | | | | | | Makefile
+| | | | | | | LICENSE.txt
+| | | | | | | Gemfile
+| | | | | | | .yardopts
+| | | | | | | .travis.yml
+| | | | | | | .gitignore
+| | | | | | | service/
+| | | | | | | internal/
+| | | | | | | doc-src/
+| | | | | | | awsmigrate/
+| | | | | | | aws/
+| | | | | | | apis/
+| | | | | | | .git/
+| | | | | aarzilli/
+| | | | | ActiveState/
+| | | | code.google.com/
+| | | | 9fans.net/
+| | | pkg/
+| | | bin/
+| | ip_failover/
+| | repos/
+| | | puppet/
+| | | lovebutton/
+| | | elbi-tools/
+| | | elbi-site/
+| | | elbi-charity-tools/
+| | | elbi-api-java/
+| | | elbi-admin/
+| | | confd/
+| | | | README.md
+| | | | MAINTAINERS
+| | | | LICENSE
+| | | | CONTRIBUTING.md
+| | | | CHANGELOG
+| | | | .travis.yml
+| | | | .gitignore
+| | | | test*
+| | | | install*
+| | | | clean*
+| | | | build*
+| | | | vendor/
+| | | | src/
+| | | | | github.com/
+| | | | | | kelseyhightower/
+| | | | | | | confd/
+| | | | | | | | version.go
+| | | | | | | | node_var.go
+| | | | | | | | config_test.go
+| | | | | | | | config.go
+| | | | | | | | confd.go
+| | | | | | | | resource/
+| | | | | | | | log/
+| | | | | | | | backends/
+| | | | | | | | | config.go
+| | | | | | | | | client.go
+| | | | | | | | | zookeeper/
+| | | | | | | | | stackengine/
+| | | | | | | | | redis/
+| | | | | | | | | etcd/
+| | | | | | | | | env/
+| | | | | | | | | dynamodb/
+| | | | | | | | | consul/
+| | | | | | | | | asg/
+| | | | | | | | | | client.go
+| | | | pkg/
+| | | | integration/
+| | | | docs/
+| | | | contrib/
+| | | | bin/
+| | | | .git/
+| | repos_old/
+| | shared/
+| | tmp/
+| | zone/
+| | .ansible_module_generated*
+| | .ICEauthority
+| | .Xauthority
+| | .bash_aliases
+| | .bash_history
+| | .bash_logout
+| | .bash_profile
+| | .bashrc
+| | .boto@
+| | .bzr.log
+| | .dmrc
+| | .git-stats
+| | .gitconfig
+| | .gitignore
+| | .install4j
+| | .lesshst
+| | .mysql_history
+| | .netExtender.log
+| | .profile
+| | .rnd
+| | .scummvmrc
+| | .steampath@
+| | .steampid@
+| | .tmux.conf
+| | .viminfo
+| | .vimrc
+| | .xsession-errors
+| | .xsession-errors.old
+| | asg.go
+| | examples.desktop
+| | .asg.go.swp
