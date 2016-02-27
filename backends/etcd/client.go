c20899658390532212758b61a3b3296d74fbb854
Rene Kaufmann <kaufmann.r@gmail.com>
2016-02-27 15:09:27 +0100

    improved etcd watch support

--- backends/etcd/client.go
+++ backends/etcd/client.go
@@ -115,49 +115,65 @@ func nodeWalk(node *client.Node, vars map[string]string) error {
 	return nil
 }
 
+type watchResponse struct {
+	waitIndex uint64
+	err       error
+}
+
 func (c *Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, stopChan chan bool) (uint64, error) {
 	// return something > 0 to trigger a key retrieval from the store
 	if waitIndex == 0 {
 		return 1, nil
 	}
 
-	for {
-		// Setting AfterIndex to 0 (default) means that the Watcher
-		// should start watching for events starting at the current
-		// index, whatever that may be.
-		watcher := c.client.Watcher(prefix, &client.WatcherOptions{AfterIndex: uint64(0), Recursive: true})
-		ctx, cancel := context.WithCancel(context.Background())
-		cancelRoutine := make(chan bool)
-		defer close(cancelRoutine)
+	ctx, cancel := context.WithCancel(context.Background())
+	cancelRoutine := make(chan bool)
+	respChan := make(chan watchResponse)
+	defer close(cancelRoutine)
 
-		go func() {
+	go func() {
+		for {
 			select {
-			case <-stopChan:
-				cancel()
 			case <-cancelRoutine:
+				cancel()
 				return
-			}
-		}()
+			default:
+				// Setting AfterIndex to 0 (default) means that the Watcher
+				// should start watching for events starting at the current
+				// index, whatever that may be.
+				watcher := c.client.Watcher(prefix, &client.WatcherOptions{AfterIndex: uint64(0), Recursive: true})
+				resp, err := watcher.Next(ctx)
+				if err != nil {
+					switch e := err.(type) {
+					case *client.Error:
+						if e.Code == 401 {
+							respChan <- watchResponse{0, nil}
+							return
+						}
+					}
+					respChan <- watchResponse{waitIndex, err}
+					return
+				}
 
-		resp, err := watcher.Next(ctx)
-		if err != nil {
-			switch e := err.(type) {
-			case *client.Error:
-				if e.Code == 401 {
-					return 0, nil
+				// Only return if we have a key prefix we care about.
+				// This is not an exact match on the key so there is a chance
+				// we will still pickup on false positives. The net win here
+				// is reducing the scope of keys that can trigger updates.
+				for _, k := range keys {
+					if strings.HasPrefix(resp.Node.Key, k) {
+						respChan <- watchResponse{resp.Node.ModifiedIndex, err}
+						return
+					}
 				}
 			}
-			return waitIndex, err
 		}
-
-		// Only return if we have a key prefix we care about.
-		// This is not an exact match on the key so there is a chance
-		// we will still pickup on false positives. The net win here
-		// is reducing the scope of keys that can trigger updates.
-		for _, k := range keys {
-			if strings.HasPrefix(resp.Node.Key, k) {
-				return resp.Node.ModifiedIndex, err
-			}
+	}()
+	for {
+		select {
+		case <-stopChan:
+			return waitIndex, nil
+		case r := <-respChan:
+			return r.waitIndex, r.err
 		}
 	}
 }
