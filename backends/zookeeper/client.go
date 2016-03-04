5e4438f2e7c2e04fee7981548ccc775181377a8b
Rene Kaufmann <kaufmann.r@gmail.com>
2016-03-04 16:31:36 +0100

    some zookeeper watch optimizations

--- backends/zookeeper/client.go
+++ backends/zookeeper/client.go
@@ -80,37 +80,29 @@ type watchResponse struct {
 	err       error
 }
 
-func (c *Client) watchFolder(folder string, respChan chan watchResponse, cancelRoutine chan bool) {
-	_, _, eventCh, err := c.client.ChildrenW(folder)
+func (c *Client) watch(key string, respChan chan watchResponse, cancelRoutine chan bool) {
+	_, _, keyEventCh, err := c.client.GetW(key)
 	if err != nil {
 		respChan <- watchResponse{0, err}
 	}
+	_, _, childEventCh, err := c.client.ChildrenW(key)
+	if err != nil {
+		respChan <- watchResponse{0, err}
+	}
+
 	for {
 		select {
-		case e := <-eventCh:
+		case e := <-keyEventCh:
+			if e.Type == zk.EventNodeDataChanged {
+				respChan <- watchResponse{1, e.Err}
+			}
+		case e := <-childEventCh:
 			if e.Type == zk.EventNodeChildrenChanged {
 				respChan <- watchResponse{1, e.Err}
 			}
 		case <-cancelRoutine:
-			// There is no way to stop ChildrenW so just quit
-			return
-		}
-	}
-}
-
-func (c *Client) watchKey(key string, respChan chan watchResponse, cancelRoutine chan bool) {
-	_, _, eventCh, err := c.client.GetW(key)
-	if err != nil {
-		respChan <- watchResponse{0, err}
-	}
-	for {
-		select {
-		case e := <-eventCh:
-			if e.Type == zk.EventNodeDataChanged {
-				respChan <- watchResponse{1, e.Err}
-			}
-		case <-cancelRoutine:
-			// There is no way to stop GetW so just quit
+			log.Debug("Stop watching: " + key)
+			// There is no way to stop GetW/ChildrenW so just quit
 			return
 		}
 	}
@@ -132,25 +124,32 @@ func (c *Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, sto
 	cancelRoutine := make(chan bool)
 	defer close(cancelRoutine)
 
-	//watch prefix for new/deleted children
-	watchMap := make(map[string]string)
-	log.Debug("Watching: " + prefix)
-	go c.watchFolder(prefix, respChan, cancelRoutine)
 	//watch all subfolders for changes
+	watchMap := make(map[string]string)
 	for k, _ := range entries {
-		for dir := filepath.Dir(k); dir != prefix; dir = filepath.Dir(dir) {
-			if _, ok := watchMap[dir]; !ok {
-				watchMap[dir] = ""
-				log.Debug("Watching: " + dir)
-				go c.watchFolder(dir, respChan, cancelRoutine)
+		for _, v := range keys {
+			if strings.HasPrefix(k, v) {
+				for dir := filepath.Dir(k); dir != "/"; dir = filepath.Dir(dir) {
+					if _, ok := watchMap[dir]; !ok {
+						watchMap[dir] = ""
+						log.Debug("Watching: " + dir)
+						go c.watch(dir, respChan, cancelRoutine)
+					}
+				}
+				break
 			}
 		}
 	}
 
 	//watch all keys in prefix for changes
 	for k, _ := range entries {
-		log.Debug("Watching: " + k)
-		go c.watchKey(k, respChan, cancelRoutine)
+		for _, v := range keys {
+			if strings.HasPrefix(k, v) {
+				log.Debug("Watching: " + k)
+				go c.watch(k, respChan, cancelRoutine)
+				break
+			}
+		}
 	}
 
 	for {
