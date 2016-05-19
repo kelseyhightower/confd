3bfa5b2a31503e8c63c2e9bb8fcda21511e5ff63
Rene Kaufmann <kaufmann.r@gmail.com>
2016-05-19 22:54:29 +0200

    etcdv3 backend

---
+++ backends/etcdv3/client.go
@@ -0,0 +1,122 @@
+package etcdv3
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+// Client is a wrapper around the etcd client
+type Client struct {
+	client *clientv3.Client
+}
+
+// NewEtcdClient returns an *etcdv3.Client with a connection to named machines.
+func NewEtcdClient(machines []string, cert, key, caCert string, basicAuth bool, username string, password string) (*Client, error) {
+	var cli *clientv3.Client
+	cfg := clientv3.Config{
+		Endpoints:   machines,
+		DialTimeout: 5 * time.Second,
+	}
+
+	tlsEnabled := false
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: false,
+	}
+
+	if caCert != "" {
+		certBytes, err := ioutil.ReadFile(caCert)
+		if err != nil {
+			return &Client{cli}, err
+		}
+
+		caCertPool := x509.NewCertPool()
+		ok := caCertPool.AppendCertsFromPEM(certBytes)
+
+		if ok {
+			tlsConfig.RootCAs = caCertPool
+		}
+		tlsEnabled = true
+	}
+
+	if cert != "" && key != "" {
+		tlsCert, err := tls.LoadX509KeyPair(cert, key)
+		if err != nil {
+			return &Client{cli}, err
+		}
+		tlsConfig.Certificates = []tls.Certificate{tlsCert}
+		tlsEnabled = true
+	}
+
+	if tlsEnabled {
+		cfg.TLS = tlsConfig
+	}
+
+	cli, err := clientv3.New(cfg)
+	if err != nil {
+		return &Client{cli}, err
+	}
+	return &Client{cli}, nil
+}
+
+// GetValues queries etcd for keys prefixed by prefix.
+func (c *Client) GetValues(keys []string) (map[string]string, error) {
+	vars := make(map[string]string)
+	for _, key := range keys {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(3)*time.Second)
+		resp, err := c.client.Get(ctx, key, clientv3.WithPrefix(), clientv3.WithSort(clientv3.SortByKey, clientv3.SortDescend))
+		cancel()
+		if err != nil {
+			return vars, err
+		}
+		for _, ev := range resp.Kvs {
+			vars[string(ev.Key)] = string(ev.Value)
+		}
+	}
+	return vars, nil
+}
+
+func (c *Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, stopChan chan bool) (uint64, error) {
+	// return something > 0 to trigger a key retrieval from the store
+	if waitIndex == 0 {
+		return 1, nil
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancelRoutine := make(chan bool)
+	defer close(cancelRoutine)
+	var err error
+
+	go func() {
+		select {
+		case <-stopChan:
+			cancel()
+		case <-cancelRoutine:
+			return
+		}
+	}()
+
+	rch := c.client.Watch(ctx, prefix, clientv3.WithPrefix())
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			fmt.Println(string(ev.Kv.Key))
+			// Only return if we have a key prefix we care about.
+			// This is not an exact match on the key so there is a chance
+			// we will still pickup on false positives. The net win here
+			// is reducing the scope of keys that can trigger updates.
+			for _, k := range keys {
+				if strings.HasPrefix(string(ev.Kv.Key), k) {
+					return uint64(ev.Kv.Version), err
+				}
+			}
+		}
+	}
+	return 0, err
+}
