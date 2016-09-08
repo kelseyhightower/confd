afed39690847af18892ab006735b342cd7470663
jolestar <jolestar@gmail.com>
2016-09-08 12:25:14 +0800

    ensure metad client watch goroutine normal quit.

--- backends/metad/client.go
+++ backends/metad/client.go
@@ -123,6 +123,8 @@ func (c *Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, sto
 		c.waitIndex = 1
 		return c.waitIndex, nil
 	}
+	done := make(chan struct{})
+	defer close(done)
 
 	req, _ := http.NewRequest("GET", fmt.Sprintf("%s%s?wait=true", c.url, prefix), nil)
 	req.Header.Set("Accept", "application/json")
@@ -131,6 +133,8 @@ func (c *Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, sto
 		select {
 		case <-stopChan:
 			c.transport.CancelRequest(req)
+		case <-done:
+			return
 		}
 	}()
 
