916ad4a0ccd0f93e8cba68064c15ebea80ef378d
chai2010 <chaishushan@gmail.com>
2018-02-24 11:36:03 +0800

    fix fileStat close when open failed on windows

--- resource/template/fileStat_windows.go
+++ resource/template/fileStat_windows.go
@@ -12,10 +12,10 @@ import (
 func fileStat(name string) (fi fileInfo, err error) {
 	if isFileExist(name) {
 		f, err := os.Open(name)
-		defer f.Close()
 		if err != nil {
 			return fi, err
 		}
+		defer f.Close()
 		stats, _ := f.Stat()
 		fi.Mode = stats.Mode()
 		h := md5.New()
