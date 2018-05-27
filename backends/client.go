59d7bb878c16204fdd73810ad9a526d4c0d5aee8
chardon2 <pascal.chardon@nokia.com>
2018-05-27 09:14:14 +0200

    Added TLS client insecure mode for etcdv2 backend.

--- backends/client.go
+++ backends/client.go
@@ -27,6 +27,7 @@ type StoreClient interface {
 
 // New is used to create a storage client based on our configuration.
 func New(config Config) (StoreClient, error) {
+
 	if config.Backend == "" {
 		config.Backend = "etcd"
 	}
@@ -50,7 +51,7 @@ func New(config Config) (StoreClient, error) {
 	case "etcd":
 		// Create the etcd client upfront and use it for the life of the process.
 		// The etcdClient is an http.Client and designed to be reused.
-		return etcd.NewEtcdClient(backendNodes, config.ClientCert, config.ClientKey, config.ClientCaKeys, config.BasicAuth, config.Username, config.Password)
+		return etcd.NewEtcdClient(backendNodes, config.ClientCert, config.ClientKey, config.ClientCaKeys, config.ClientInsecure, config.BasicAuth, config.Username, config.Password)
 	case "etcdv3":
 		return etcdv3.NewEtcdClient(backendNodes, config.ClientCert, config.ClientKey, config.ClientCaKeys, config.BasicAuth, config.Username, config.Password)
 	case "zookeeper":
