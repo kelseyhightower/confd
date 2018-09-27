15f9917bb1d46a14d04174c565a41b17ea009d1d
Mike Morris <michael.m.morris@pearson.com>
2018-09-26 19:54:14 -0500

    Implement WatchPrefix() for dotenv backend
    Use file backend as a starting point

--- backends/dotenv/client.go
+++ backends/dotenv/client.go
@@ -1,15 +1,23 @@
 package dotenv
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/confd/log"
 )
 
 // Client provides a shell for the env client
 type Client struct {
 	filePath string
+}
+
+type ResultError struct {
+	response uint64
+	err      error
 }
 
 // NewDotenvClient returns a new client
@@ -48,8 +56,45 @@ func (c *Client) GetValues(keys []string) (map[string]string, error) {
 	return vars, nil
 }
 
-// WatchPrefix is not implemented
 func (c *Client) WatchPrefix(prefix string, keys []string, waitIndex uint64, stopChan chan bool) (uint64, error) {
-	<-stopChan
-	return 0, nil
+	if waitIndex == 0 {
+		return 1, nil
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return 0, err
+	}
+	defer watcher.Close()
+
+	watcher.Add(c.filePath)
+
+	output := c.watchChanges(watcher, stopChan)
+	if output.response != 2 {
+		return output.response, output.err
+	}
+	return waitIndex, nil
+}
+
+func (c *Client) watchChanges(watcher *fsnotify.Watcher, stopChan chan bool) ResultError {
+	outputChannel := make(chan ResultError)
+	go func() error {
+		defer close(outputChannel)
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Debug(fmt.Sprintf("Event: %s", event))
+				if event.Op&fsnotify.Write == fsnotify.Write ||
+					event.Op&fsnotify.Remove == fsnotify.Remove ||
+					event.Op&fsnotify.Create == fsnotify.Create {
+					outputChannel <- ResultError{response: 1, err: nil}
+				}
+			case err := <-watcher.Errors:
+				outputChannel <- ResultError{response: 0, err: err}
+			case <-stopChan:
+				outputChannel <- ResultError{response: 1, err: nil}
+			}
+		}
+	}()
+	return <-outputChannel
 }
