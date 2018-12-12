b163f257c1c8e9716e801928957e7c2515a57520
Britt Treece <britt.treece@gmail.com>
2018-12-11 22:04:42 -0600

    Define ClientInsecure

--- config.go
+++ config.go
@@ -24,15 +24,16 @@ type BackendsConfig = backends.Config
 type Config struct {
 	TemplateConfig
 	BackendsConfig
-	Interval      int    `toml:"interval"`
-	SecretKeyring string `toml:"secret_keyring"`
-	SRVDomain     string `toml:"srv_domain"`
-	SRVRecord     string `toml:"srv_record"`
-	LogLevel      string `toml:"log-level"`
-	Watch         bool   `toml:"watch"`
-	PrintVersion  bool
-	ConfigFile    string
-	OneTime       bool
+	Interval       int    `toml:"interval"`
+	SecretKeyring  string `toml:"secret_keyring"`
+	SRVDomain      string `toml:"srv_domain"`
+	SRVRecord      string `toml:"srv_record"`
+	LogLevel       string `toml:"log-level"`
+	Watch          bool   `toml:"watch"`
+	PrintVersion   bool
+	ConfigFile     string
+	OneTime        bool
+	ClientInsecure bool
 }
 
 var config Config
@@ -44,7 +45,7 @@ func init() {
 	flag.StringVar(&config.ClientCaKeys, "client-ca-keys", "", "client ca keys")
 	flag.StringVar(&config.ClientCert, "client-cert", "", "the client cert")
 	flag.StringVar(&config.ClientKey, "client-key", "", "the client key")
-        flag.BoolVar(&config.ClientInsecure, "client-insecure", false, "Allow connections to SSL sites without certs (only used with -backend=etcd)")
+	flag.BoolVar(&config.ClientInsecure, "client-insecure", false, "Allow connections to SSL sites without certs (only used with -backend=etcd)")
 	flag.StringVar(&config.ConfDir, "confdir", "/etc/confd", "confd conf directory")
 	flag.StringVar(&config.ConfigFile, "config-file", "/etc/confd/confd.toml", "the confd config file")
 	flag.Var(&config.YAMLFile, "file", "the YAML file to watch for changes (only used with -backend=file)")
