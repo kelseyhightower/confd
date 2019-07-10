772974f344818aa56ce5ca6e5a50f93e6369e9d1
Britt Treece <britt.treece@gmail.com>
2019-07-09 20:59:06 -0500

    Bump version to v0.17.2-dev
    [skip ci]

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package main
 
-const Version = "0.17.1"
+const Version = "0.17.2-dev"
 
 // We want to replace this variable at build time with "-ldflags -X main.GitSHA=xxx", where const is not supported.
 var GitSHA = ""
