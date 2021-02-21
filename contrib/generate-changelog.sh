c3e72aaf34650e5da9de91b2c633f543e644a308
abtreece <britt.treece@gmail.com>
2021-02-21 11:57:43 -0600

    Update versions and CHANGELOG for 0.18 release

--- contrib/generate-changelog.sh
+++ contrib/generate-changelog.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Generates a changelog of all merges from a given release all the way to HEAD.
 
-REPO=https://github.com/kelseyhightower/confd
+REPO=https://github.com/abtreece/confd
 
 usage() {
     echo "Usage: $0 <FROM> [TO]"
@@ -19,4 +19,4 @@ TO=${2:-HEAD}
 
 printf "### $TO\n\n"
 
-git --no-pager log --merges --format="%h %b" $FROM..$TO
+git log --pretty="%h - %s (%an)" $FROM..$TO
--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package main
 
-const Version = "0.17.3-dev"
+const Version = "0.18.0"
 
 // We want to replace this variable at build time with "-ldflags -X main.GitSHA=xxx", where const is not supported.
 var GitSHA = ""
