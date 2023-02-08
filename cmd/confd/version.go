e8be2881d107600e84e9680c5dd67e87cf0a6a0f
Britt Treece <britt@spreedly.com>
2023-02-08 16:48:55 -0500

    set version for 0.19.2

--- cmd/confd/version.go
+++ cmd/confd/version.go
@@ -1,6 +1,6 @@
 package main
 
-const Version = "0.19.2-dev"
+const Version = "0.19.2"
 
 // We want to replace this variable at build time with "-ldflags -X main.GitSHA=xxx", where const is not supported.
 var GitSHA = ""
